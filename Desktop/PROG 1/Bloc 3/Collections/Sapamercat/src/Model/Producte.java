813acd4547e786103a53727d93425e0838d6ba68
Mija <c.candelaresi@sapalomera.cat>
2026-03-18 11:59:11 +0100

    Afegits models Producte i Textil amb getters/setters i Main actualitzat

--- Desktop/PROG 1/Bloc 3/Collections/Sapamercat/src/Model/Producte.java
+++ Desktop/PROG 1/Bloc 3/Collections/Sapamercat/src/Model/Producte.java
@@ -1,4 +1,30 @@
 package Model;
 
-public class Producte {
+public abstract class Producte {
+
+    private String nom;
+    private double preu;
+    private String codiBarres;
+
+    public Producte(String nom, double preu, String codiBarres){
+        this.nom = nom;
+        this.preu = preu;
+        this.codiBarres = codiBarres;
+    }
+
+    public String getNom(){
+        return nom;
+    }
+
+    public double getPreu(){
+        return preu;
+    }
+
+    public String getCodiBarres(){
+        return codiBarres;
+    }
+
+    // He posat abstracte el metod calcularPreu perquè cada tipus de producte tindra una manera diferent de calcular el preu.
+    public abstract double calcularPreu();
+
 }
--- Desktop/PROG 1/Bloc 3/Collections/Sapamercat/src/Model/Textil.java
+++ Desktop/PROG 1/Bloc 3/Collections/Sapamercat/src/Model/Textil.java
@@ -1,4 +1,20 @@
 package Model;
 
-public class Textil {
+public class Textil extends Producte{
+
+    private String composicio;
+
+    public Textil(String nom, double preu, String codiBarres, String composicio){
+        super(nom, preu, codiBarres);
+        this.composicio = composicio;
+    }
+
+    public String getComposicio(){
+        return composicio;
+    }
+
+    @Override
+    public double calcularPreu(){
+        return getPreu();
+    }
 }
