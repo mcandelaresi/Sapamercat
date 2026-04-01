5ff35433c3e5c08cb44217d942ac3c2e8a3ec436
Mijael Candelaresi <c.candelaresi@sapalomera.cat>
2026-04-01 18:09:34 +0200

    Comentar codi de producte

--- Desktop/PROG 1/Bloc 3/Collections/Sapamercat/src/Model/Producte.java
+++ Desktop/PROG 1/Bloc 3/Collections/Sapamercat/src/Model/Producte.java
@@ -1,23 +1,31 @@
 package Model;
 
+/**
+ * Aquesta és la classe abstracta Producte.
+ * He decidit fer-la abstracta perquè hi haurà diferents tipus de productes
+ * (Alimentació, Textil, Electrònica) i cada un calcularà el preu de manera diferent.
+ */
 public abstract class Producte implements Comparable<Producte>{
 
-    protected String nom;
-    protected double preu;
-    protected String codiBarres;
+    // Atributs comuns a tots els productes
+    protected String nom;        // He posat el nom com a String per identificar cada producte
+    protected double preu;       // El preu base del producte
+    protected String codiBarres; // Utilitzo el codi de barres per diferenciar productes iguals
 
+    // Constructor per inicialitzar els atributs
     public Producte(String nom, double preu, String codiBarres){
         this.nom = nom;
         this.preu = preu;
         this.codiBarres = codiBarres;
     }
 
+    // Getters i setters per accedir i modificar els atributs
     public String getNom() {
         return nom;
     }
 
     public void setNom(String nom) {
-        this.nom = nom;
+        this.nom = nom; // Actualitzo el nom del producte
     }
 
     public double getPreu() {
@@ -25,7 +33,7 @@ public abstract class Producte implements Comparable<Producte>{
     }
 
     public void setPreu(double preu) {
-        this.preu = preu;
+        this.preu = preu; // Actualitzo el preu del producte
     }
 
     public String getCodiBarres() {
@@ -33,12 +41,17 @@ public abstract class Producte implements Comparable<Producte>{
     }
 
     public void setCodiBarres(String codiBarres) {
-        this.codiBarres = codiBarres;
+        this.codiBarres = codiBarres; // Assigno un nou codi de barres
     }
 
-    // He posat abstracte el metod calcularPreu perquè cada tipus de producte tindra una manera diferent de calcular el preu.
+    // He posat abstracte el mètode calcularPreu perquè cada tipus de producte tindrà la seva manera de calcular-lo
     public abstract double calcularPreu();
 
+    /**
+     * Implemento Comparable per poder ordenar productes per nom.
+     * He decidit ordenar-los alfabèticament perquè em sembla mes facil aixi.
+     */
+    @Override
     public int compareTo(Producte o) {
         return this.nom.compareTo(o.nom);
     }
